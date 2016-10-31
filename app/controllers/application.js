8d8d196c80abacaa8eb8c431ac81a3476f43312f
gewethor@github.com <gwethor@gmail.com>
2016-10-30 20:45:25 -0700

    jQuery added

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -62,19 +62,60 @@ export default Ember.Controller.extend({
 		addrow: function(){
 			var table = document.getElementById("ITable");
 			var row = table.insertRow(table.length);
+			row.className = "data";
 			var cell1 = row.insertCell(0);
 			var cell2 = row.insertCell(1);
 			var cell3 = row.insertCell(2);
 			var cell4 = row.insertCell(3);
+			var cell5 = row.insertCell(4);
 			cell1.innerHTML = "Insert Date(MM/DD/YYYY)";
+			cell1.className = "inputValue"
 			cell2.innerHTML = "Insert Description";
+			cell2.className = "inputValue"
 			cell3.innerHTML = "Insert Amount($0.00)";
-			cell4.innerHTML = "<button contenteditable='false' type='button' {{action 'delrow'}}> Delete </button>";
+			cell3.className = "inputValue"
+			cell4.innerHTML = "<ul id='list'><select id='categories' class='inputValue' name='categories'><option>Select Option</option><option>Fast Food</option><option>Happy Hour</option></select></ul>";
+			cell5.innerHTML = "<button contenteditable='false' type='button' {{action 'deleteRow' this}}> Delete </button>";
 		},
-		delrow: function(){
-			var row = document.getElementByTagName("tr");
-			var spot = row.rowIndex;
-			document.getElementById("ITable").deleteRow(spot);
+		deleteRow: function(r){
+			var i = r.parentNode.parentNode.rowIndex;
+			document.getElementById('ITable').deleteRow(i);
+		},
+		submit: function(){
+			var data = $("#ITable tr.data").map(function (index, elem) {
+				var ret = [];
+				$('.inputValue', this).each(function () {
+					var d = $(this).val() || $(this).text();
+					ret.push(d);
+					console.log(d);
+				});
+				return ret;
+			});
+			console.log(data);
+			var ftable = document.getElementById("ITable").innerHTML;
+			console.log(ftable);
 		}
+		 
+		/*  
+		dateValid: function (dateString) {
+			if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(dateString))
+        	return false;
+
+    		var parts = dateString.split("/");
+    		var day = parseInt(parts[1], 10);
+    		var month = parseInt(parts[0], 10);
+    		var year = parseInt(parts[2], 10);
+
+    		if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        	return false;
+
+    		var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+		    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        	monthLength[1] = 29;
+
+    		return day > 0 && day <= monthLength[month - 1];
+			} 
+			*/
 	}
 });
